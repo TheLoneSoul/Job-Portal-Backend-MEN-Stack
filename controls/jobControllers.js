d91cf491b5e416af177c8b50be9bf1160b887996
TheLoneSoul <aabhasb25@gmail.com>
2026-04-23 19:15:12 +0545

    feat: improve get job response handling

--- controls/jobControllers.js
+++ controls/jobControllers.js
@@ -1,4 +1,16 @@
-// GET all jobs - initial read-only implementation (CRUD in progress)
-exports.getAllJobs = (req, res) => {
-    res.send("Job fetched successfully");
+const Jobs = require('../models/Jobs')
+
+exports.getAllJobs = async(req, res, next) => {
+    try {
+        const job = await Jobs.find();
+        if(job.length === 0){
+            res.status(404).json({
+            success: false,
+            message: "Job not found"
+            })
+        }
+        res.status(200).json(job);
+    } catch (error) {
+        next(error)
+    }
 };
