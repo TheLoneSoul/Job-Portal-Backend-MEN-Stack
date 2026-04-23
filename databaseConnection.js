60753bbb5062e08d24c9618641db5064e507def0
TheLoneSoul <aabhasb25@gmail.com>
2026-04-23 14:31:14 +0545

    chore: connect mongodb database

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,1 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
+databaseURI = process.env.mongoDatabaseURI;
+
+const databaseConnection = async() => {
+    try {
+        const connect = await mongoose.connect(databaseURI, {
+            serverSelectionTimeoutMS: 20000
+        });
+        console.log(`Database Connected: ${connect.connection.host}`);
+        
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+module.exports = databaseConnection;
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { log } = require('node:console');
+const databaseConnection = require('./databaseConnection.js');
+require('dotenv').config();
 const app = express();
-
+databaseConnection();
 
 app.use(express.json());
 
 app.use('/api', (req, res) => {
     res.send('API is working');
-})
+});
 
 app.listen(3000, () => {
     console.log("Server is running");
     
-})
+});
