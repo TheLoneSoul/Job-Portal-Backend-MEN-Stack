bfdfbae86e7ff85dd50f6dfb7fde04b656f1fcb0
TheLoneSoul <aabhasb25@gmail.com>
2026-05-03 15:32:37 +0545

    feat: protect job routes with auth middleware

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const jobs = require('./jobRoutes.js');
 const User = require('./userRoute.js')
+const {protection} = require('../middleware/authMiddleware.js')
 
 router.use('/user', User);
-router.use('/jobs', jobs);
+router.use('/jobs', protection, jobs);
 module.exports = router;
