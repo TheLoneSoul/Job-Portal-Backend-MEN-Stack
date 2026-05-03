1fb1df041b076f6a7437d70f5ac1339046bb2889
TheLoneSoul <aabhasb25@gmail.com>
2026-05-03 15:27:55 +0545

    feat: implement auth protection middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,22 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.protection = async(req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith("Bearer")){
+           return res.status(401).json({success: false, message: "Unauthorize: No token provided!"})
+        };
+        const token = authHeader.split(" ")[1];
+        const verification = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findById(verification.userId).select('-password');
+        if(!user){
+            return res.status(404).json({success: false, message: "User not found"})
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error)
+    }
+};
