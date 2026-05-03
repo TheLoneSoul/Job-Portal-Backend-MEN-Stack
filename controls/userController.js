668f5c11a4fd424d5f1739e5ecf0cbafac28b8b6
TheLoneSoul <aabhasb25@gmail.com>
2026-05-03 14:59:54 +0545

    feat: add loginUser controller with JWT authentication

--- controls/userController.js
+++ controls/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 exports.registerUser = async(req, res, next) => {
     try {
         const {email, password, name} = req.body;
@@ -20,6 +20,24 @@ exports.registerUser = async(req, res, next) => {
         next(error);
     }
 };
+
+exports.signinUser = async(req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        const isUser = await User.findOne({email});
+        if(!isUser){
+            return res.status(404).json({success: false, message: "Invalid Credential: User with this email doesn't exist."})
+        }
+        const comparePassword = await bcrypt.compare(password, isUser.password);
+        if(!comparePassword){
+            return res.status(401).json({success: false, message: "Invalid Credential: Password didn't match"});
+        }
+        const token = jwt.sign({userId: isUser._id, email: isUser.email, name: isUser.name}, process.env.SECRET_KEY, {expiresIn:'1d'})
+        res.status(200).json({success: true, token, message: "Login Successful"})
+    } catch (error) {
+        next(error);
+    }
+}
 
 exports.getUser = async(req, res, next) => {
     try {
