2a2e5240d690add09c55d7003cafc1a58f29cbd4
TheLoneSoul <aabhasb25@gmail.com>
2026-05-03 21:19:10 +0545

    feat: protect user route with auth and isAdmin middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {getUser, registerUser, signinUser} = require('../controls/userController')
+const {protection, isAdmin } = require('../middleware/authMiddleware')
 
-router.get('/',getUser);
+router.get('/', protection, isAdmin, getUser);
 router.post('/register', registerUser);
 router.post('/signin', signinUser);
 module.exports = router;
